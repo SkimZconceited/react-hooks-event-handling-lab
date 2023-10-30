0d8aff3ff90f92a9e9b074cb12dc0ca84576d53a
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-10-30 15:34:57 +0300

    Completed asssignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+
+    function handleFocus(event) {
+        console.log('Good!')
+    }
+
+    function handleBlur(event) {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <>
+        <input type="submit" value={'Eyes on me'} onFocus={handleFocus} onBlur={handleBlur} ></input>
+        </>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function handleChange(event) {
+        return Boolean(event.target.value) === true ? console.log('Entering password...') : console.warn('Shouldnt get here!')
+    }
+
     return (
-        <div></div>
+        <div><input type="password" onChange={handleChange} placeholder="Type something..." ></input></div>
     )
 }
 
